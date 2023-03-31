783c41e5def5fcb0bcdfa63c13e8e85f3019f72c
PRABHAKARAN <100403697+PRABHAKARAN-R-ML@users.noreply.github.com>
2023-03-31 11:00:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,5 @@
-resource "aws_instance" "demo_ec2"{
-  ami           = var.Ami_id
-  instance_type = var.instance_type
-  root_block_device {
-    volume_size           = var.root_block_size
-    volume_type           = var.root_block_type
-    encrypted             = true
-    delete_on_termination = true
+Provider "aws" {
 
-  }
-  ebs_block_device {
-    device_name           = "/dev/xvda"
-    volume_size           = var.ebs_block_size
-    volume_type           = var.ebs_block_type
-    encrypted             = "true"
-    delete_on_termination = true
-  }
 }
 terraform {
   backend "s3" {
